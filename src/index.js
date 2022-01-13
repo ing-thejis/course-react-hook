d9733f8ea6458fd423b281a60858553f53536560
ing-thejis <ing.jesithgarcia@gmail.com>
2022-01-13 12:19:52 -0500

    adding useLayoutEffect

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import ReactDOM from 'react-dom';
 //import SimpleForm from './components/02-useEffect/SimpleForm'
 //import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook'
 //import MultipleCustomHook from './components/03-example/MultipleCustomHook'
-import FocusScreen from './components/04-useRef/FocusScreen'
+//import FocusScreen from './components/04-useRef/FocusScreen'
+import Layout from './components/05-useLayoutEffect/Layout'
 
 ReactDOM.render(
-    <FocusScreen />,
+    <Layout />,
   document.getElementById('root')
 );
